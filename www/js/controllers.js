e8e268b928017e422c4d997c23c712a304d3ff2a
njerucyrus <njerucyrus5@gmail.com>
2016-09-30 07:49:23 +0300

    initial commit

---
+++ www/js/controllers.js
@@ -0,0 +1,73 @@
+angular.module('starter.controllers', [])
+
+.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+
+  // With the new view caching in Ionic, Controllers are only called
+  // when they are recreated or on app start, instead of every page change.
+  // To listen for when this page is active (for example, to refresh data),
+  // listen for the $ionicView.enter event:
+  //$scope.$on('$ionicView.enter', function(e) {
+  //});
+
+  // Form data for the login modal
+  $scope.loginData = {};
+
+  // Create the login modal that we will use later
+  $ionicModal.fromTemplateUrl('templates/login.html', {
+    scope: $scope
+  }).then(function(modal) {
+    $scope.modal = modal;
+  });
+
+  // Triggered in the login modal to close it
+  $scope.closeLogin = function() {
+    $scope.modal.hide();
+  };
+
+  // Open the login modal
+  $scope.login = function() {
+    $scope.modal.show();
+  };
+
+  // Perform the login action when the user submits the login form
+  $scope.doLogin = function() {
+    console.log('Doing login', $scope.loginData);
+
+    // Simulate a login delay. Remove this and replace with your login
+    // code if using a login system
+    $timeout(function() {
+      $scope.closeLogin();
+    }, 1000);
+  };
+})
+
+.controller('PostsCtrl', function($scope, $http) {
+  $scope.posts = [];
+  $http.get('http://localhost:8000/api/posts/').success(function(response){
+   angular.forEach(response, function(response){
+    console.log(response);
+    $scope.posts.push(response);
+   });
+  });
+
+})
+.controller('CreateAccountCtrl', function($scope, $http){
+    $scope.account_data = {};
+
+    $scope.createAccount = function(){
+      
+      $data = $scope.account_data;
+      $http.post('http://localhost:8000/api/create-account/', $data).success(function(response){
+        console.log("api response", response.message);
+        alert(response.message);
+    });
+
+
+    console.log("Creating account", $scope.account_data);
+  };
+  
+})
+
+
+.controller('Playlists', function($scope, $stateParams) {
+});
